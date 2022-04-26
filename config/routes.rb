782a4703fa8eda7cff941acb4c6f7c0c8a296550
Hollie Hensman <hensmanh@gmail.com>
2022-04-26 02:41:23 +0000

    Define route and generate controller for admin/categories

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
-  resources :categories, only: [:show]
+
+  namespace :admin do
+  resources :categories, only: [:show, :index, :create]
+  end
 
   resource :cart, only: [:show] do
     post   :add_item
