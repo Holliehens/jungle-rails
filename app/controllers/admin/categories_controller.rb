425f206d18fd6011fecc5f29eb2abf4b5a78bd15
Hollie Hensman <hensmanh@gmail.com>
2022-04-26 20:41:02 +0000

    Categories new.html.erb form refactor

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,17 +6,26 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def new
-    @categories = Category.new
+    @category = Category.new
+    puts "CATEGORY #{@category}"
   end
 
   def create
-    @categories = Category.new(category_params)
+    @category = Category.new(category_params)
 
-    if @categories.save
+    if @category.save
       redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 
 end
