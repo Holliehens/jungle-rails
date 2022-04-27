b0ad57999537ae6cd06ef70323818129cde4c3a3
Hollie Hensman <hensmanh@gmail.com>
2022-04-27 19:56:13 +0000

    Create a users controller. Add a new action (for rendering the signup form) and a create action (for receiving the form and creating a user with the form's parameters.)

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,12 @@ Rails.application.routes.draw do
   end
 
   get '/about', to: 'about#index'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -76,4 +82,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
